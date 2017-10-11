9aef3d1441d3d7c9fc8b077ec90f2173e44594a2
illumitata <gothic8282@gmail.com>
2017-10-11 02:23:58 +0200

    last one left

--- Exercism/ruby/nth-prime/nth_prime.rb
+++ Exercism/ruby/nth-prime/nth_prime.rb
@@ -1,7 +1,40 @@
 class Prime
 
   def self.nth(number)
+    
+    if(number==0)
+      return puts("Error")
+    else
+      wynik = 0
+      lp = 0
+      prime = 2
+      t = true
+
+      while lp<number do
+
+        t = true
+
+        for i in 2...prime-1
+          if prime % i == 0
+            t = false
+            break
+          end
+        end
+
+        if t == true
+          wynik = prime
+          lp += 1
+        end
+        prime += 1
+      end
+
+      return wynik
+    end
 
   end
 
+end
+
+module BookKeeping
+  VERSION = 1
 end
